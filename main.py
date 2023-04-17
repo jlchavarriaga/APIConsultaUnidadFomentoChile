d25c842fc6d7f40d48330db2330e96e517e50102
Jorge Chavarriaga <jlchavarriaga@gmail.com>
2023-04-17 15:21:39 -0500

    Se agregan mejoras al codigo y la documentacion

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+
 from datetime import datetime
 from bs4 import BeautifulSoup
 
 import requests
 import calendar
 import locale
-import re
-import json
 
 app = FastAPI()
 
@@ -21,10 +21,12 @@ async def root():
 
 @app.get('/api/{date}')
 def api(date: str):
-    
-    date_obj = datetime.strptime(date, '%d-%m-%Y')
-    
-    #Obtenemos el año de la fecha suministrada, para determinar que url debemos consultar 
+    try:
+        date_obj = datetime.strptime(date, '%d-%m-%Y')
+    except ValueError:
+        raise HTTPException(status_code=400, detail='Invalid date format')
+
+    #Obtenemos el año, el mes y el dia de la fecha suministrada, para determinar que url debemos consultar 
     year = date_obj.year
     month = date_obj.month
     day = date_obj.day
@@ -32,9 +34,8 @@ def api(date: str):
     # Establecemos la localización a español, para que el nombre del mes NO sea retornado en ingles
     locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
 
-    #Obtenemos el nombre del mes tomando el numero
+    #Obtenemos el nombre del mes tomando el numero respectivo (Enero, 1 ; Febrero, 2 etc)
     nameMonth=  calendar.month_name[month] 
-    #nameMonth = datetime.strptime(str(month), "%m").strftime("%B")   #Opcion 2
     
     if date_obj: 
         #En el sitio web, si el año es anterior al 2012 entonces la consulta debe realizarse desde otro link
@@ -42,17 +43,15 @@ def api(date: str):
             url = 'https://www.sii.cl/valores_y_fechas/uf/uf'+year.__str__()+'.htm'
         else:
             url= f'https://www.sii.cl/pagina/valores/uf/uf{year}.htm'
-            return {'error': 'Date not available (< 2013)'}
+            #para este caso no realizaremos la consulta anterior, pero aca se podria realizar otro tipo de busqueda
+            raise HTTPException(status_code=404, detail='Date not available (< 2013)')
         
         #Capturamos la respuesta de la solicitud
         response = requests.get(url,headers=encabezado)
 
-        if response.status_code == 200:
-          # return response.json()
-          # return response.text
-       
-            #Debemos recorrer la respuesta en busqueda del valor solicitado
-            # ya sea por id (id='mes_mayo')  o por el nombre del mes con la etiqueta <h2>(Mayo)   
+        if response.status_code == 200:       
+           #Debemos recorrer la respuesta en busqueda del valor solicitado
+           # ya sea por id (id='mes_mayo')  o por el nombre del mes con la etiqueta <h2>(Mayo)   
             
            idMont= f'mes_{nameMonth}'   #obtenemos el nombre del mes y generamos el id a buscar
            soup = BeautifulSoup(response.text)
@@ -72,19 +71,19 @@ def api(date: str):
                 if valores[i-1].isdigit():
                     diccionario[int(valores[i-1])] = valores[i]
            
-           #return diccionario aca devolvemos todos los valores del mes 
+           #return diccionario #aca devolvemos todos los valores del mes, seria ideal colocar en otro end point
            
-           valor = diccionario[day]
-           
-           #return valor devolvermos solo el valor 
+           try:
+                valor = diccionario[day]
+           except KeyError:
+                raise HTTPException(status_code=404, detail='Date not available')
+
            
            # Creamos un diccionario con el valor del día
            resultado = {'dia': date.__str__(), 'valor': valor}
            
            # Convertimos el diccionario a formato JSON
-           resultado_json = json.dumps(resultado)
-           print (resultado_json)
-           print (resultado)
+           resultado_json = jsonable_encoder(resultado)
            
            return resultado_json
            
